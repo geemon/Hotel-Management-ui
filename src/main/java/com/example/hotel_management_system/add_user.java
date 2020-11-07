75c9513422bdf1865a7fcccc95dae67aa6c91051
geemon <38504190+geemon@users.noreply.github.com>
2020-11-07 11:08:57 +0000

    Add files via upload

---
+++ src/main/java/com/example/hotel_management_system/add_user.java
@@ -0,0 +1,14 @@
+package com.example.hotel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_user);
+    }
+}
---
+++ src/main/java/com/example/hotel_management_system/service_reservation.java
@@ -0,0 +1,14 @@
+package com.example.hotel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class service_reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_reservation);
+    }
+}
